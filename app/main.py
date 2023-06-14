77b228bde5ee69b17f57412e04a83fe5e9eec8c1
Abhi-1504 <abhilash.raj.me@gmail.com>
2023-06-14 17:09:03 +0530

    ADD: Course Overiew endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dbloader import MongoDbConn
+from typing import Optional
 
 app = FastAPI()
 mongo_db_conn = MongoDbConn()
@@ -8,5 +9,9 @@ mongo_db_conn = MongoDbConn()
 
 
 @app.get("/courses/{sort_by}", description="Endpoint to get all the courses sorted")
-async def get_all_courses(sort_by : str):
-    return mongo_db_conn.get_all_courses(sort_by)
+async def get_all_courses(sort_by : str, domain: Optional[str] = None):
+    return mongo_db_conn.get_all_courses(sort_by, domain)
+
+@app.get("/course_overview", description="Endpoint to get an overiew of a course")
+async def get_course_overiew(course_name: str):
+    return mongo_db_conn.get_course_overview(course_name)
